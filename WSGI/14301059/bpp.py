3aa81124ecae33a9e49c025e987481bea3557b13
GodDongS <14301059@bjtu.edu.cn>
2016-09-26 23:33:18 +0800

    Update bpp.py

--- WSGI/14301059/bpp.py
+++ WSGI/14301059/bpp.py
@@ -1,1 +1,17 @@
+from flask import Flask
+from flask import request
+
+app=Flask(__name__)
+
+@app.route('/aaa',methods=['GET','POST'])
+def home():
+	return '<h1>hello aaa</h1>'
+
+
+@app.route('/hello.html',methods=['GET','POST'])
+def erro():
+	return '<h1>404 NOT FOUND</h1>'
+
+if __name__ == '__main__':
+	app.run()
 
